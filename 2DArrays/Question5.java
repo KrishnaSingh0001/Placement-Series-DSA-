0357d6d5ba7e88f46d3425c939cdbc2eff3f3bc8
Krishna Singh <singhkrishna5099213@gmail.com>
2024-08-05 16:50:43 +0530

    5th commit

--- 2DArrays/Question5.java
+++ 2DArrays/Question5.java
@@ -14,5 +14,18 @@ public class Question5 {
             }
             System.out.println();
         }
+        System.out.println();
+
+
+        // Storing 
+
+        int [][] transpose = new int [n][m];
+        for( int  i = 0 ; i<n ; i++){
+            for(int j = 0 ; j<m ; j++){
+                transpose[i][j] = arr[j][i];
+                System.out.print(transpose[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
