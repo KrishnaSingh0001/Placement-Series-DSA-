7f350f9da5110bebc464784303de9c67fa881b3c
Krishna Singh <singhkrishna5099213@gmail.com>
2024-08-06 17:13:15 +0530

    6th day commit

---
+++ 2DArrays/Question7.java
@@ -0,0 +1,28 @@
+// Write a program to rotate the matrix by 90degree clock wise . 
+
+public class Question7 {
+    public static void print(int [][] arr) {
+        int m = arr.length;
+        for(int i = 0 ; i<m ; i++){
+            for(int j = 0 ; j<m ; j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int [][] arr = {{1,2,3},{4,5,6},{7,8,9}};
+        int m = arr.length;
+
+        print(arr);
+        for(int i = 0 ; i< m ; i++){
+            for(int j = 0 ; j<=i; j++){
+                int temp = arr[i][j];
+                arr[i][j]= arr[j][i];
+                arr[j][i] = temp;
+            }
+        }
+        print(arr);
+    }
+}
