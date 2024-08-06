8833baac0649b368eecf572ac944eeb20f77b9c9
Krishna Singh <singhkrishna5099213@gmail.com>
2024-08-06 17:31:50 +0530

    6th Day commit

--- 2DArrays/Question8.java
+++ 2DArrays/Question8.java
@@ -1,7 +1,8 @@
-// Transpose of the matrix of of the arrays 
+// Write a program to rotate the matrix by 90degree clock wise . 
 
-public class Question7 {
-    public static void print(int [][] arr) {
+
+public class Question8 {
+    public static void print (int [][] arr) {
         int m = arr.length;
         for(int i = 0 ; i<m ; i++){
             for(int j = 0 ; j<m ; j++){
@@ -12,7 +13,7 @@ public class Question7 {
         System.out.println();
     }
     public static void main(String[] args) {
-        int [][] arr = {{1,2,3},{4,5,6},{7,8,9}};
+        int [][] arr  = {{1 , 2 , 3 },{4,5,6},{7 , 8, 9}};
         int m = arr.length;
 
         print(arr);
@@ -21,6 +22,17 @@ public class Question7 {
                 int temp = arr[i][j];
                 arr[i][j]= arr[j][i];
                 arr[j][i] = temp;
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            int left = 0;
+            int right = m - 1;
+            while (left < right) {
+                int temp = arr[i][left];
+                arr[i][left] = arr[i][right];
+                arr[i][right] = temp;
+                left++;
+                right--;
             }
         }
         print(arr);
