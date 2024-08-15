2f10bdb62206441b14643012f184ab7b0e748aaf
Krishna Singh <singhkrishna5099213@gmail.com>
2024-08-16 01:28:10 +0530

    10th day

---
+++ 2DArrays/MultiplicationOfTwoMatrices.java
@@ -0,0 +1,5 @@
+public class MultiplicationOfTwoMatrices {
+    public static void main(String[] args) {
+        
+    }
+}
--- String/BasicOfString.java
+++ String/BasicOfString.java
@@ -1,7 +1,19 @@
 package String;
-
+import java.util.*;
 public class BasicOfString {
     public static void main(String args []){
-        str = "Krishna Singh"
+        char arr[] = {'a','b','c','d'};
+        String str1=("abcd");
+        String str2 = new String("xyz");
+
+
+
+        // String are IMMUTABLE (Means not changeable)
+        Scanner sc = new Scanner(System.in);
+        // String name = sc.next();
+        String name = sc.nextLine();
+        System.out.println(name.length());
+
+        
     }
 }
