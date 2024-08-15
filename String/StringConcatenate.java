690733b02e7c9bac1a81a3fb172552c7454513c0
Krishna Singh <singhkrishna5099213@gmail.com>
2024-08-16 02:14:53 +0530

    10th Commit of the Day

---
+++ String/StringConcatenate.java
@@ -0,0 +1,10 @@
+package String;
+import java.util.*;
+public class StringConcatenate {
+    public static void main(String[] args) {
+        String firstName = "Krishna";
+        String SecondName = "Singh";
+        String fullName = firstName + " "+ SecondName;
+        System.out.println( fullName + " is this " );
+    }
+}
