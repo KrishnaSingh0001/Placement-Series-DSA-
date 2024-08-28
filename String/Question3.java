005d91ca9188e49dcc0b34af4600d486de8d3c7b
Krishna Singh <singhkrishna5099213@gmail.com>
2024-08-29 01:03:25 +0530

    14th day commit

---
+++ String/Question3.java
@@ -0,0 +1,8 @@
+// Take integer input and convert it into a String 
+package String;
+public class Question3 {
+    public static void main(String [] args){
+        
+
+    }
+}
---
+++ String/convertIntToString.java
@@ -0,0 +1,11 @@
+package String;
+import java.util.*;
+public class convertIntToString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Enter a number : ");
+        int n = sc.nextInt();
+        String s =" " + n;
+        System.out.print(s);
+    }
+}
