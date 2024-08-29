c7609197f6f7f59175a0738ee963360f7a0a6e1b
Krishna Singh <singhkrishna5099213@gmail.com>
2024-08-29 10:04:51 +0530

    15th commit

--- String/Question3.java
+++ String/Question3.java
@@ -2,7 +2,9 @@
 package String;
 public class Question3 {
     public static void main(String [] args){
-        
+        int n = 1234;
+        String s = "" + n ;
+        System.out.println(s.length());
 
     }
 }
