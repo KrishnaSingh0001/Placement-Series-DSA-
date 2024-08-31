bf89fa3089a325bcce0e3833dde67ef85ef7236f
Krishna Singh <singhkrishna5099213@gmail.com>
2024-09-01 00:27:28 +0530

    15th commit

--- String/reverseEachWordInSentence.java
+++ String/reverseEachWordInSentence.java
@@ -6,8 +6,18 @@ public class reverseEachWordInSentence {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         StringBuilder sb = new StringBuilder();
-        reverse(sb , 2 , 4);
-        System.out.println(sb);
+        // reverse(sb , 2 , 4);
+        // System.out.println(sb);
+        int n = sb.length();
+        int i = 0 , j=0;
+        while(j<n){
+            if(sb.charAt(j)!= ' '){
+                j++;
+            }
+            else{
+                reverse(sb , 2 , 4);
+            }
+        }
 
     }
 
