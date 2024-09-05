70569dadbcdbeb1e3f49eeac67f0020531b8631c
Krishna Singh <singhkrishna5099213@gmail.com>
2024-09-05 14:52:19 +0530

    19th day commit

--- BasicSortingAlgorithm/bubblesort.java
+++ BasicSortingAlgorithm/bubblesort.java
@@ -14,21 +14,17 @@ public class bubblesort {
         int n = arr.length;
         print(arr);
         for(int x = 0 ; x<=n-1;x++){
+            boolean flag = true;  //Check if this pas has sorted or not
             for(int i = 0 ; i<n-1-x;i++){
                 if(arr[i]>=arr[i+1]){
                     int temp = arr[i];
                     arr[i] = arr[i+1];
                     arr[i+1]= temp;
-                }
-            }
-            //Check if this pas has sorted or not 
-            boolean flag = true;
-            for(int i = 0 ; i<n-1-x;i++){
-                if(arr[i]>arr[i+1]){
                     flag = false;
-                    break;
                 }
             }
+            if(flag==true) break;
+            
         }
         print(arr);
     }
