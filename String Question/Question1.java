294abb8c006d5101c5c5b10d72addbbb18cef4f1
Krishna Singh <singhkrishna5099213@gmail.com>
2024-09-18 15:28:57 +0530

    20th Day Commit

--- String Question/Question1.java
+++ String Question/Question1.java
@@ -1,11 +1,12 @@
-// package Strings;
-
-public class stringCompression {
+// String Compression Question (All FANG Company Question )
+// In this Question we compress the String and know the length of the String 
+public class Question1 {
     public static void main(String[] args) {
-        String s = "aaaaannbbbuuuucccc";
+        String s = "aabbccc";
         char[] arr = s.toCharArray();
         String ans = " ";
         int i = 0 , j = 0 ;
+        int l = s.length();
         while(j<arr.length){
             if(arr[j]==arr[i]){
                 j++;
@@ -26,5 +27,19 @@ public class stringCompression {
         ans +=len;
         }
         System.out.println(ans);
+        System.out.println(l);
     }
+    // System.out.println(l);
 }
+// public class Question1 {
+//     public static void main(String[] args) {
+//         int n = char.size();
+//         int index = 0;
+//         int i = 0 ;
+//         while(i<n){
+//             char curr_char = char [i];
+//             count =0;
+
+//         }
+//     }
+// }
