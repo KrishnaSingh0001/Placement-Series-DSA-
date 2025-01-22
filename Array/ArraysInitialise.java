a320b1d9f3927d72a2b3f43573591599126352ee
Krishna Singh <singhkrishna5099213@gmail.com>
2025-01-22 19:08:34 +0530

    1st

--- Array/ArraysInitialise.java
+++ Array/ArraysInitialise.java
@@ -1,25 +1,46 @@
-import java.util.*;
-public class ArraysInitialise {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Array Size :  ");
-        int n = sc.nextInt();
+// import java.util.*;
+// public class ArraysInitialise {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter Array Size :  ");
+//         int n = sc.nextInt();  // for takin  input as a user  
         
-        int arr[] = new int [n]; // Size of Array
-        // int n = arr.length;  // length of Arrray (Length operator )
-        // System.out.println(n);
-        /// for input looop
-        for (int i = 0 ; i<= n-1 ; i++){
-            System.out.print("Enter the element of Array");
+//         int arr[] = new int [n]; // Size of Array
+//         // int n = arr.length;  // length of Arrray (Length operator )
+//         // System.out.p7\]\]rintln(n);
+//         /// for input looop
+//         for (int i = 0 ; i<= n-1 ; i++){
+//             System.out.print("Enter the element of Array");
+//             arr[i] = sc.nextInt();
+//         }
+
+//         ///For output looop
+
+//         for(int i = 0 ; i<=n-1;i++){
+//             System.out.println(arr[i] + " ");
+//         }
+        
+//     }
+    
+// }
+
+import java.util.*;
+
+public class ArraysInitialise {
+    public static void main(String [] args ){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr [] = new  int [n];
+
+        for(int i = 0 ; i < n ; i++){
+            System.out.println("Enter the element of the Array ");
             arr[i] = sc.nextInt();
+
         }
-
-        ///For output looop
-
-        for(int i = 0 ; i<=n-1;i++){
+        for(int i = 0 ; i<=n-1; i++){
             System.out.println(arr[i] + " ");
         }
-        
+
+
     }
-    
 }
